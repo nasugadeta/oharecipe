33f1b07b95167b944af893a989db75e25d0e003a
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-10 18:55:37 +0900

    rails g devise:views adminsを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  def name
-    family_name + given_name
-  end
+
 end
