2596cdd57195863912aadf86677b6a52b59b29b4
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-10 19:29:43 +0900

    ログイン画面や編集画面を英語表記から日本語表記などに。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,11 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
+    #user新規登録
     devise_parameter_sanitizer.for(:sign_up) << :family_name
     devise_parameter_sanitizer.for(:sign_up) << :given_name
-
+    #user編集
+    # devise_parameter_sanitizer.for(:edit) << :family_name
+    # devise_parameter_sanitizer.for(:edit) << :given_name
   end
 end
