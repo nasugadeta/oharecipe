ffb77efd5e97b51b615a3df6e66a13252ded65ef
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-10 11:47:02 +0900

    recipes_controllerに各メソッドを追加

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,35 @@
 class RecipesController < ApplicationController
+  class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  def new
+    @recipe = Recipe.new
+  end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to "/"
+  end
+  def index
+    @recipes = Recipe.all
+  end
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to "/"
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(
+      :name,
+      :explain,
+      :admin_id,
+      :restaurant_id)
+  end
+end
 end
