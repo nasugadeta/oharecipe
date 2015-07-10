e5a610522cf9b5641152565922c77c05b2da8f32
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-10 13:51:47 +0900

    recipeアップデート後、recipe/:idにリダイレクトする機能を実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,7 +19,7 @@ class RecipesController < ApplicationController
   def update
     @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
-    redirect_to "/"
+    redirect_to :action => "show", :id => @recipe
   end
 
   private
