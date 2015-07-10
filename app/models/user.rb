31809265db65e30ef870062257b0df5a0b99ddd0
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-11 00:40:57 +0900

    family_name+given_nameでnameをlayoutで表示できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  def name
+    family_name + given_name
+  end
 end
