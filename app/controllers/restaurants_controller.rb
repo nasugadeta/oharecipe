b24abdfe96fc0f8af0d7a32f0f15ca40dcef9c32
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-10 12:17:35 +0900

    restaurants_controllerに各メソッドを追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,33 @@
 class RestaurantsController < ApplicationController
+  before_action
+  def new
+    @restaurant = Restaurant.new
+  end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to "/"
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def index
+    @restaurants = Restaurant.all
+  end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to "/"
+  end
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :address,
+      :explain,
+      :tel)
+  end
 end
