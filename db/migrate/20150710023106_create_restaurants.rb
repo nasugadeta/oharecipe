24563cb6a608ea1cc4e92cd73378f0c6d38d2d42
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-07-10 11:40:43 +0900

    recipe,restaurantのmigration fileに"name", "explain"を追加

--- db/migrate/20150710023106_create_restaurants.rb
+++ db/migrate/20150710023106_create_restaurants.rb
@@ -1,7 +1,8 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-
+      t.string :name
+      t.string :explain
       t.timestamps null: false
     end
   end
--- db/migrate/20150710023159_create_recipes.rb
+++ db/migrate/20150710023159_create_recipes.rb
@@ -1,7 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :explain
       t.timestamps null: false
     end
   end
